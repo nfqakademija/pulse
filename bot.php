c830a5302e1d5b2b9a01c34d7261456d571aeb3c
am <miklovisarvydas@gmail.com>
2019-11-17 14:19:31 +0200

    added slcak bot

---
+++ bot.php
@@ -0,0 +1,35 @@
+<?php
+require 'vendor/autoload.php';
+use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use React\EventLoop\Factory;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Slack\SlackRTMDriver;
+// Load driver
+DriverManager::loadDriver(SlackRTMDriver::class);
+$loop = Factory::create();
+$botman = BotManFactory::createForRTM([
+    'slack' => [
+        'token' => 'xoxb-811436599713-809425196483-EC0mz4DcHps9AjnMw0STD5u0',
+    ],
+], $loop);
+$botman->hears('test', function($bot) {
+    $question = Question::create('How are you')
+        ->fallback('Done')
+        ->callbackId('q1')
+        ->addButtons([
+            Button::create('Good')->value('yes'),
+            Button::create('IDK')->value('idk'),
+            Button::create('Not good')->value('no'),
+        ]);
+    $answer = '';
+    $bot->ask($question, function (Answer $response, $bot) {
+        $bot->say('Hi ' . $response->getText());
+    });
+});
+// $botman->say('HELLO MA FRIEND', 'UQ3AJBYSH');
+$loop->run();
+?>
+© 2019 GitHub, Inc.
