ba56a604b7a3063227be37939a270adeaa4f4810
Andrius Inčiura <andrius.inciura@gmail.com>
2019-12-17 22:10:40 +0200

    Fresh migration

---
+++ src/Migrations/Version20191217200354.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191217200354 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, poll_id INT DEFAULT NULL, question_number INT NOT NULL, question LONGTEXT NOT NULL, INDEX IDX_B6F7494E3C947C0F (poll_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE answer (id INT AUTO_INCREMENT NOT NULL, responder_id VARCHAR(255) DEFAULT NULL, survey_id INT DEFAULT NULL, answer_option_id INT DEFAULT NULL, value VARCHAR(255) NOT NULL, INDEX IDX_DADD4A2537395ADB (responder_id), INDEX IDX_DADD4A25B3FE509D (survey_id), INDEX IDX_DADD4A259A3BC2B9 (answer_option_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE responder (slack_id VARCHAR(255) NOT NULL, team_lead_id INT DEFAULT NULL, email VARCHAR(180) DEFAULT NULL, slack_username VARCHAR(255) DEFAULT NULL, department VARCHAR(255) DEFAULT NULL, job_title VARCHAR(255) DEFAULT NULL, full_name VARCHAR(255) DEFAULT NULL, site VARCHAR(255) DEFAULT NULL, team VARCHAR(255) DEFAULT NULL, INDEX IDX_5F311AF7FF2C34BA (team_lead_id), PRIMARY KEY(slack_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE survey (id INT AUTO_INCREMENT NOT NULL, poll_id INT DEFAULT NULL, datetime DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_AD5F9BFC3C947C0F (poll_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE poll (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_84BCFA45A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `option` (id INT AUTO_INCREMENT NOT NULL, question_id INT DEFAULT NULL, value VARCHAR(255) NOT NULL, INDEX IDX_5A8600B01E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E3C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A2537395ADB FOREIGN KEY (responder_id) REFERENCES responder (slack_id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A25B3FE509D FOREIGN KEY (survey_id) REFERENCES survey (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A259A3BC2B9 FOREIGN KEY (answer_option_id) REFERENCES `option` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE responder ADD CONSTRAINT FK_5F311AF7FF2C34BA FOREIGN KEY (team_lead_id) REFERENCES user (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE survey ADD CONSTRAINT FK_AD5F9BFC3C947C0F FOREIGN KEY (poll_id) REFERENCES poll (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE poll ADD CONSTRAINT FK_84BCFA45A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE `option` ADD CONSTRAINT FK_5A8600B01E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `option` DROP FOREIGN KEY FK_5A8600B01E27F6BF');
+        $this->addSql('ALTER TABLE responder DROP FOREIGN KEY FK_5F311AF7FF2C34BA');
+        $this->addSql('ALTER TABLE poll DROP FOREIGN KEY FK_84BCFA45A76ED395');
+        $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A2537395ADB');
+        $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A25B3FE509D');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E3C947C0F');
+        $this->addSql('ALTER TABLE survey DROP FOREIGN KEY FK_AD5F9BFC3C947C0F');
+        $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A259A3BC2B9');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE answer');
+        $this->addSql('DROP TABLE responder');
+        $this->addSql('DROP TABLE survey');
+        $this->addSql('DROP TABLE poll');
+        $this->addSql('DROP TABLE `option`');
+    }
+}
